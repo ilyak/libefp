fa4b2214c915cf013212192fd03e800e4dd09674
Ilya Kaliman <ilya.kaliman@gmail.com>
2012-05-04 20:31:11 +0000

    Test for all terms. All EFP/EFP terms work.

---
+++ tests/total_1a.c
@@ -0,0 +1,125 @@
+/*-
+ * Copyright (c) 2012 Ilya Kaliman
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include "test_common.h"
+
+static const char *potential_files[] = {
+	ABS_TOP_SRCDIR "/fraglib/h2o.efp",
+	ABS_TOP_SRCDIR "/fraglib/nh3.efp",
+	NULL
+};
+
+static const char *fragname[] = {
+	"H2O_L",
+	"NH3_L",
+	 NULL
+};
+
+static const double xyzabc[] = {
+	BOHR(0.0), BOHR(0.0), BOHR(0.0), 1.0, 2.0, 3.0,
+	BOHR(5.0), BOHR(0.0), BOHR(0.0), 5.0, 2.0, 8.0
+};
+
+static const double ref_gradient[] = { /* from Q-Chem 4.0 */
+	0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+	0.0, 0.0, 0.0, 0.0, 0.0, 0.0
+};
+
+static enum efp_result
+overlap_integrals_fn(struct efp_xr_block *block, double *s, double *sx,
+		     void *user_data)
+{
+	static const int basis_size = 140;
+
+	if (block->basis_size_i != basis_size ||
+	    block->basis_size_j != basis_size)
+		return EFP_RESULT_INVALID_ARRAY_SIZE;
+
+	int size = block->basis_size_i * block->basis_size_j;
+
+	FILE *fp = fopen(ABS_TOP_SRCDIR "/tests/data/sint_1", "r");
+	if (!fp)
+		return EFP_RESULT_FILE_NOT_FOUND;
+
+	for (int i = 0; i < size; i++, s++)
+		fscanf(fp, "%lf", s);
+
+	fclose(fp);
+
+	if (sx)
+		return EFP_RESULT_NOT_IMPLEMENTED;
+
+	return EFP_RESULT_SUCCESS;
+}
+
+static enum efp_result
+kinetic_integrals_fn(struct efp_xr_block *block, double *t, double *tx,
+		     void *user_data)
+{
+	static const int basis_size = 140;
+
+	if (block->basis_size_i != basis_size ||
+	    block->basis_size_j != basis_size)
+		return EFP_RESULT_INVALID_ARRAY_SIZE;
+
+	int size = block->basis_size_i * block->basis_size_j;
+
+	FILE *fp = fopen(ABS_TOP_SRCDIR "/tests/data/tint_1", "r");
+	if (!fp)
+		return EFP_RESULT_FILE_NOT_FOUND;
+
+	for (int i = 0; i < size; i++, t++)
+		fscanf(fp, "%lf", t);
+
+	fclose(fp);
+
+	if (tx)
+		return EFP_RESULT_NOT_IMPLEMENTED;
+
+	return EFP_RESULT_SUCCESS;
+}
+
+static const struct test_data test_data = {
+	.potential_files = potential_files,
+	.fragname = fragname,
+	.xyzabc = xyzabc,
+		/* elec + pol + disp + xr from Q-Chem 4.0 */
+	.ref_energy =  0.000255425403 + -0.000010402834 +
+		      -0.000098903256 +  0.000013466610,
+	.ref_gradient = ref_gradient,
+	.opts = {
+		.terms = EFP_TERM_ELEC | EFP_TERM_POL |
+			 EFP_TERM_DISP | EFP_TERM_XR,
+		.disp_damp = EFP_DISP_DAMP_TT,
+		.do_gradient = 0
+	},
+	.callbacks = {
+		.get_overlap_integrals = overlap_integrals_fn,
+		.get_kinetic_integrals = kinetic_integrals_fn
+	}
+};
+
+DEFINE_TEST(test_data)
