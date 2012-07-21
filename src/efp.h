6a6573dd8bbdefff3f751a0df12ab5a47768a348
Ilya Kaliman <ilya.kaliman@gmail.com>
2012-07-21 14:35:28 -0400

    Move mainpage to separate file.

--- src/efp.h
+++ src/efp.h
@@ -31,27 +31,6 @@
  * Public libefp interface.
  */
 
-/** \mainpage libefp - The Effective Fragment Potential method implementation
- *
- * \section API Public API Documentation
- *
- * The library is still in active development and API is UNSTABLE.
- * Documentation for public API is available <a href="efp_8h.html">here</a>.
- * For additional information see
- * <a href="http://github.com/libefp/libefp/blob/master/README.md">README</a>
- * file.
- *
- * \section Repo Git Repository
- *
- * Latest development version of code can be found in git
- * <a href="http://github.com/libefp/libefp">repository</a>.
- *
- * \copyright Copyright (c) 2012 Ilya Kaliman.
- * Distributed under the terms of BSD 2-clause license. See
- * <a href="http://github.com/libefp/libefp/blob/master/LICENSE">LICENSE</a>
- * file for details.
- */
-
 #ifdef __cplusplus
 extern "C" {
 #endif
