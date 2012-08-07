6509b4d122353a9acba91f00a007a7f20b1e59e8
Ilya Kaliman <ilya.kaliman@gmail.com>
2012-08-07 16:09:47 -0400

    CG module fixes.

--- efpmd/cg.c
+++ efpmd/cg.c
@@ -78,15 +78,16 @@ static int check_conv(double rms_grad, double max_grad, double opt_tol)
 	return fabs(max_grad) < opt_tol && fabs(rms_grad) < opt_tol / 3.0;
 }
 
-static void print_status(struct efp *efp, double e_diff, double rms_grad,
-			 double max_grad)
+static void print_status(struct efp *efp, double e_diff,
+		double rms_grad, double max_grad)
 {
 	print_geometry(efp);
 	print_energy(efp);
 
-	printf("ENERGY DIFFERENCE FROM PREVIOUS STEP %20.10lf\n", e_diff);
-	printf("        ROOT MEAN SQUARE OF GRADIENT %20.10lf\n", rms_grad);
-	printf("          MAXIMUM GRADIENT COMPONENT %20.10lf\n", max_grad);
+	printf("                ENERGY CHANGE = %16.10lf\n", e_diff);
+	printf("                 RMS GRADIENT = %16.10lf\n", rms_grad);
+	printf("             MAXIMUM GRADIENT = %16.10lf\n", max_grad);
+	printf("\n\n");
 }
 
 void sim_cg(struct efp *efp, const struct config *config)
@@ -97,45 +98,54 @@ void sim_cg(struct efp *efp, const struct config *config)
 	if ((res = efp_get_frag_count(efp, &n_frag)))
 		lib_error(res);
 
-	size_t n_grad = 6 * n_frag;
-	double coord[n_grad], grad[n_grad];
+	size_t n_coord = 6 * n_frag;
+	double coord[n_coord], grad[n_coord];
 	double e_new, e_old;
 
-	struct opt_state *state = opt_create(n_grad);
+	if ((res = efp_get_coordinates(efp, n_coord, coord)))
+		lib_error(res);
+
+	struct opt_state *state = opt_create(n_coord);
 	if (!state)
 		error("UNABLE TO CREATE AN OPTIMIZER");
 
 	opt_set_fn(state, energy_fn);
+	opt_set_ls_step_size(state, config->ls_step_size);
+	opt_set_user_data(state, efp);
 
-	if (!opt_init(state, coord))
+	if (opt_init(state, coord))
 		error("UNABLE TO INITIALIZE AN OPTIMIZER");
 
 	e_old = e_new = opt_get_fx(state);
 
 	for (int step = 1; step <= config->max_steps; step++) {
-		if (!opt_step(state))
+		if (opt_step(state))
 			error("UNABLE TO MAKE AN OPTIMIZATION STEP");
 
-		opt_get_gx(state, n_grad, grad);
+		opt_get_gx(state, n_coord, grad);
+		e_new = opt_get_fx(state);
+
+		if (e_new == NAN)
+			error("ENERGY IS NAN");
 
 		double e_diff = e_new - e_old;
 		double rms_grad = 0.0;
 		double max_grad = 0.0;
 
-		for (size_t i = 0; i < n_grad; i++) {
+		for (size_t i = 0; i < n_coord; i++) {
 			rms_grad += grad[i] * grad[i];
 
 			if (fabs(grad[i]) > max_grad)
 				max_grad = grad[i];
 		}
 
-		rms_grad = sqrt(rms_grad / n_grad);
+		rms_grad = sqrt(rms_grad / n_coord);
 
 		if (check_conv(rms_grad, max_grad, config->opt_tol)) {
-			printf("\nOPTIMIZATION CONVERGED\n\n");
 			printf("FINAL ENERGY AND GEOMETRY\n\n");
 			print_status(efp, e_diff, rms_grad, max_grad);
-			return;
+			printf("OPTIMIZATION CONVERGED\n");
+			break;
 		}
 
 		if (step % config->print_step == 0) {
@@ -145,4 +155,6 @@ void sim_cg(struct efp *efp, const struct config *config)
 
 		e_old = e_new;
 	}
+
+	opt_shutdown(state);
 }
