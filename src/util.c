4b3d00c5f2aa8a505fc26329aafc6e8f778d22df
Ilya Kaliman <ilya.kaliman@gmail.com>
2017-04-21 16:29:37 -0700

    Use same style as rest of the code.

--- src/util.c
+++ src/util.c
@@ -29,7 +29,8 @@
 #include "private.h"
 #include "util.h"
 
-int efp_skip_frag_pair(const struct efp *efp, size_t fr_i_idx, size_t fr_j_idx)
+int
+efp_skip_frag_pair(const struct efp *efp, size_t fr_i_idx, size_t fr_j_idx)
 {
 	size_t idx = fr_i_idx * efp->n_frag + fr_j_idx;
 
@@ -49,14 +50,14 @@ int efp_skip_frag_pair(const struct efp *efp, size_t fr_i_idx, size_t fr_j_idx)
 		vec_t cell = { efp->box.x * round(dr.x / efp->box.x),
 			       efp->box.y * round(dr.y / efp->box.y),
 			       efp->box.z * round(dr.z / efp->box.z) };
-
 		dr = vec_sub(&dr, &cell);
 	}
 
 	return (vec_len_2(&dr) > cutoff2);
 }
 
-struct swf efp_make_swf(const struct efp *efp, const struct frag *fr_i,
+struct swf
+efp_make_swf(const struct efp *efp, const struct frag *fr_i,
     const struct frag *fr_j)
 {
 	struct swf swf;
@@ -72,7 +73,6 @@ struct swf efp_make_swf(const struct efp *efp, const struct frag *fr_i,
 		swf.cell.x = efp->box.x * round(swf.dr.x / efp->box.x);
 		swf.cell.y = efp->box.y * round(swf.dr.y / efp->box.y);
 		swf.cell.z = efp->box.z * round(swf.dr.z / efp->box.z);
-
 		swf.dr.x -= swf.cell.x;
 		swf.dr.y -= swf.cell.y;
 		swf.dr.z -= swf.cell.z;
@@ -90,7 +90,8 @@ struct swf efp_make_swf(const struct efp *efp, const struct frag *fr_i,
 	return swf;
 }
 
-int efp_check_rotation_matrix(const mat_t *rotmat)
+int
+efp_check_rotation_matrix(const mat_t *rotmat)
 {
 	vec_t ax = { rotmat->xx, rotmat->yx, rotmat->zx };
 	vec_t ay = { rotmat->xy, rotmat->yy, rotmat->zy };
@@ -114,7 +115,8 @@ int efp_check_rotation_matrix(const mat_t *rotmat)
 	return 1;
 }
 
-void efp_points_to_matrix(const double *pts, mat_t *rotmat)
+void
+efp_points_to_matrix(const double *pts, mat_t *rotmat)
 {
 	vec_t p1 = { pts[0], pts[1], pts[2] };
 	vec_t p2 = { pts[3], pts[4], pts[5] };
@@ -148,16 +150,17 @@ void efp_points_to_matrix(const double *pts, mat_t *rotmat)
 	rotmat->zz = cross.z;
 }
 
-const struct frag *efp_find_lib(struct efp *efp, const char *name)
+const struct frag *
+efp_find_lib(struct efp *efp, const char *name)
 {
 	for (size_t i = 0; i < efp->n_lib; i++)
 		if (efp_strcasecmp(efp->lib[i]->name, name) == 0)
 			return efp->lib[i];
-
 	return NULL;
 }
 
-void efp_add_stress(const vec_t *dr, const vec_t *force, mat_t *stress)
+void
+efp_add_stress(const vec_t *dr, const vec_t *force, mat_t *stress)
 {
 #ifdef _OPENMP
 #pragma omp critical
@@ -175,7 +178,8 @@ void efp_add_stress(const vec_t *dr, const vec_t *force, mat_t *stress)
 	}
 }
 
-void efp_add_force(six_t *grad, const vec_t *com, const vec_t *pt,
+void
+efp_add_force(six_t *grad, const vec_t *com, const vec_t *pt,
     const vec_t *force, const vec_t *add)
 {
 	vec_t dr = vec_sub(CVEC(pt->x), com);
@@ -186,12 +190,12 @@ void efp_add_force(six_t *grad, const vec_t *com, const vec_t *pt,
 		torque.y += add->y;
 		torque.z += add->z;
 	}
-
 	six_atomic_add_xyz(grad, force);
 	six_atomic_add_abc(grad, &torque);
 }
 
-void efp_sub_force(six_t *grad, const vec_t *com, const vec_t *pt,
+void
+efp_sub_force(six_t *grad, const vec_t *com, const vec_t *pt,
     const vec_t *force, const vec_t *add)
 {
 	vec_t dr = vec_sub(CVEC(pt->x), com);
@@ -202,12 +206,12 @@ void efp_sub_force(six_t *grad, const vec_t *com, const vec_t *pt,
 		torque.y += add->y;
 		torque.z += add->z;
 	}
-
 	six_atomic_sub_xyz(grad, force);
 	six_atomic_sub_abc(grad, &torque);
 }
 
-void efp_move_pt(const vec_t *com, const mat_t *rotmat,
+void
+efp_move_pt(const vec_t *com, const mat_t *rotmat,
     const vec_t *pos_int, vec_t *out)
 {
 	*out = mat_vec(rotmat, pos_int);
@@ -216,7 +220,8 @@ void efp_move_pt(const vec_t *com, const mat_t *rotmat,
 	out->z += com->z;
 }
 
-void efp_rotate_t2(const mat_t *rotmat, const double *in, double *out)
+void
+efp_rotate_t2(const mat_t *rotmat, const double *in, double *out)
 {
 	for (size_t i = 0; i < 3 * 3; i++)
 		out[i] = 0.0;
@@ -226,12 +231,15 @@ void efp_rotate_t2(const mat_t *rotmat, const double *in, double *out)
 		for (size_t a2 = 0; a2 < 3; a2++)
 		for (size_t b2 = 0; b2 < 3; b2++)
 			out[a2 * 3 + b2] += in[a1 * 3 + b1] *
-					mat_get(rotmat, a2, a1) *
-					mat_get(rotmat, b2, b1);
+			    mat_get(rotmat, a2, a1) *
+			    mat_get(rotmat, b2, b1);
 }
 
-void efp_rotate_t3(const mat_t *rotmat, const double *in, double *out)
+void
+efp_rotate_t3(const mat_t *rotmat, const double *in, double *out)
 {
+	double a, b, c;
+
 	for (size_t i = 0; i < 3 * 3 * 3; i++)
 		out[i] = 0.0;
 
@@ -240,34 +248,37 @@ void efp_rotate_t3(const mat_t *rotmat, const double *in, double *out)
 	for (size_t c1 = 0; c1 < 3; c1++)
 		for (size_t a2 = 0; a2 < 3; a2++)
 		for (size_t b2 = 0; b2 < 3; b2++)
-		for (size_t c2 = 0; c2 < 3; c2++)
-			out[a2 * 9 + b2 * 3 + c2] += in[a1 * 9 + b1 * 3 + c1] *
-					mat_get(rotmat, a2, a1) *
-					mat_get(rotmat, b2, b1) *
-					mat_get(rotmat, c2, c1);
+		for (size_t c2 = 0; c2 < 3; c2++) {
+			a = mat_get(rotmat, a2, a1);
+			b = mat_get(rotmat, b2, b1);
+			c = mat_get(rotmat, c2, c1);
+			out[a2 * 9 + b2 * 3 + c2] +=
+			    in[a1 * 9 + b1 * 3 + c1] * a * b * c;
+		}
 }
 
-size_t efp_inner_count(size_t i, size_t n)
+size_t
+efp_inner_count(size_t i, size_t n)
 {
 	return (n % 2 ? (n - 1) / 2 : i < n / 2 ? n / 2 : n / 2 - 1);
 }
 
-int efp_strcasecmp(const char *s1, const char *s2)
+int
+efp_strcasecmp(const char *s1, const char *s2)
 {
 	while (tolower(*s1) == tolower(*s2++))
 		if (*s1++ == '\0')
 			return 0;
-
 	return tolower(*s1) - tolower(*--s2);
 }
 
-int efp_strncasecmp(const char *s1, const char *s2, size_t n)
+int
+efp_strncasecmp(const char *s1, const char *s2, size_t n)
 {
 	if (n != 0) {
 		do {
 			if (tolower(*s1) != tolower(*s2++))
 				return tolower(*s1) - tolower(*--s2);
-
 			if (*s1++ == '\0')
 				break;
 		} while (--n != 0);
